573160b5dd8b29a2b29927eeeb889860b10a6f42
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2022-09-04 21:46:42 +0100

    Delete 1_initial_migration.js

--- migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-
-const contract  = artifacts.require ("./Example.sol");
-
-module.exports = async(deployer) => {
-  var deployedContract = await contract.new() 
-  console.log(deployedContract.address)
-};
-
